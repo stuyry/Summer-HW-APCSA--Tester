42f3a1add67a4e5ae6f9e470ceae03104a903a29
Ryan Bergman <rbergman80@stuy.edu>
2026-09-10 00:43:49 -0400

    feat: forgot to add main. Need to debug my laptop bcs running java File.java is the only way to run it. Normally i recall it being java File

--- Tester.java
+++ Tester.java
@@ -16,4 +16,18 @@ public class Tester {
 			if (c == n) return i;	
 		}
 	}
+
+	public static void main(String[] args) {
+		int[] testCases = {1, 2, 5, 6, 8, 10, 13, 15, 20, 30};
+		int[] expectedResults = {2, 3, 11, 12, 19, 29, 41, 47, 71, 113};
+
+		for (int i = 0; i < testCases.length; i++) {
+			int result = nthPrime(testCases[i]);
+			if (result == expectedResults[i]) {
+				System.out.println("Test case " + testCases[i] + " passed.");
+			} else{
+				System.out.println("fail. Input: " + testCases[i] + ". Expected " + expectedResults[i] + ", but value returned was " + result);
+			}
+		}
+	}
 }
